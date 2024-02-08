01cfabb092b826151445a45fddafb49da56b8c33
stbrumm <stbrumm@web.de>
2024-02-08 13:37:31 +0100

    Initial commit

---
+++ src/main/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplication.java
@@ -0,0 +1,13 @@
+package biz.brumm.thenursejavaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNurseJavaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNurseJavaAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.brumm.thenursejavaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNurseJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
