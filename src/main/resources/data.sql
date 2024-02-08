54fe8be81dba4baa2283da01fd50251b548a5302
stbrumm <stbrumm@web.de>
2024-02-08 15:01:32 +0100

    aktueller Stand

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO countries (id, name, email) VALUES (1, 'USA', 'ab@mail.com');
+INSERT INTO countries (id, name, email) VALUES (2, 'France', 'ab@mail.com');
+INSERT INTO countries (id, name, email) VALUES (3, 'Brazil', 'ab@mail.com');
+INSERT INTO countries (id, name, email) VALUES (4, 'Italy', 'ab@mail.com');
+INSERT INTO countries (id, name, email) VALUES (5, 'Canada', 'ab@mail.com');
