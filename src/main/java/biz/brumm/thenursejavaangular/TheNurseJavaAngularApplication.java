807ea3143e792ce4cc5a35ac94427552c7ebb67e
stbrumm <stbrumm@web.de>
2024-02-08 13:58:41 +0100

    Debugging

--- src/main/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplication.java
+++ src/main/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplication.java
@@ -1,13 +1,30 @@
 package biz.brumm.thenursejavaangular;
 
+import biz.brumm.thenursejavaangular.entity.IUserRepository;
+import biz.brumm.thenursejavaangular.entity.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class TheNurseJavaAngularApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheNurseJavaAngularApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(IUserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
 	}
 
 }
--- src/main/java/biz/brumm/thenursejavaangular/entity/User.java
+++ src/main/java/biz/brumm/thenursejavaangular/entity/User.java
@@ -19,5 +19,8 @@ public class User {
 
     }
 
+    public User(String name, String s) {
+    }
+
     // standard constructors / setters / getters / toString
 }
