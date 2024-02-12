6e470b4d4c907e15ee8084cef76625ba3391ff89
stbrumm <stbrumm@web.de>
2024-02-12 07:41:22 +0100

    updated

--- src/main/java/biz/brumm/thenursejavaangular/entity/IMandantMapper.java
+++ src/main/java/biz/brumm/thenursejavaangular/entity/IMandantMapper.java
@@ -4,13 +4,9 @@ import biz.brumm.thenursejavaangular.entity.dto.MandantDTO;
 import org.mapstruct.Mapper;
 
 @Mapper
-public class IMandantMapper {
+public interface IMandantMapper {
 
-    public Mandant toMandant(MandantDTO mandantDTO) {
-        Mandant mandant = new Mandant();
-        mandant.setName(mandantDTO.getName());
-        mandant.setEmail(mandantDTO.getEmail());
-        return mandant;
-    }
+    public Mandant toMandant(MandantDTO mandantDTO);
+
 
 }
