3bd16745dc547b092b48910225d84d875568ade7
stbrumm <stbrumm@web.de>
2024-02-12 09:06:11 +0100

    assert added

--- src/main/java/biz/brumm/thenursejavaangular/entity/IMandantMapper.java
+++ src/main/java/biz/brumm/thenursejavaangular/entity/IMandantMapper.java
@@ -2,8 +2,9 @@ package biz.brumm.thenursejavaangular.entity;
 
 import biz.brumm.thenursejavaangular.entity.dto.MandantDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface IMandantMapper {
 
     public Mandant toMandant(MandantDTO mandantDTO);
--- src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
+++ src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
@@ -5,15 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TheNurseJavaAngularApplicationTests {
 
-	@Test
-	void contextLoads() {
-		assertEquals(1,1);
-	}
+
 
 }
