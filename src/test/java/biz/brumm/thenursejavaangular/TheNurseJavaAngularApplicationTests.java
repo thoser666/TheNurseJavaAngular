89295a525427e7f0314d1facc448e0a0c9475ed2
stbrumm <stbrumm@web.de>
2024-02-12 09:40:22 +0100

    Coverage increased

--- src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
+++ src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
@@ -1,9 +1,16 @@
 package biz.brumm.thenursejavaangular;
 
+import biz.brumm.thenursejavaangular.entity.IUserRepository;
+import biz.brumm.thenursejavaangular.entity.Mandant;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +19,25 @@ import static org.springframework.test.web.servlet.result.StatusResultMatchersEx
 @SpringBootTest
 @AutoConfigureMockMvc
 class TheNurseJavaAngularApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    IUserRepository iUserRepository;
+
+    Mandant mandant = new Mandant("alex", "a@bc.de");
+
+
+    @Test
+    void saveMandant()
+    {
+        String name = "alex";
+
+        Mandant erg = iUserRepository.save(mandant);
+
+        assertEquals(name, erg.getName());
+    }
 
 
 
