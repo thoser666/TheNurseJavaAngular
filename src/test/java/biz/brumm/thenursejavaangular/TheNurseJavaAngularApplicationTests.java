42b155698ef40334ffb619f2d64ff266729b80bd
stbrumm <stbrumm@web.de>
2024-02-12 08:42:04 +0100

    assert added

--- src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
+++ src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
@@ -1,8 +1,11 @@
 package biz.brumm.thenursejavaangular;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -10,6 +13,7 @@ class TheNurseJavaAngularApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1,1);
 	}
 
 }
