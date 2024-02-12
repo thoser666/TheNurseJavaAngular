cfec2cbbcacb1c382f28b7cc9a2d4a105955b2d2
stbrumm <stbrumm@web.de>
2024-02-12 07:06:48 +0100

    MandantDTO added

--- src/main/java/biz/brumm/thenursejavaangular/entity/IMandantMapper.java
+++ src/main/java/biz/brumm/thenursejavaangular/entity/IMandantMapper.java
@@ -1,8 +1,16 @@
 package biz.brumm.thenursejavaangular.entity;
 
+import biz.brumm.thenursejavaangular.entity.dto.MandantDTO;
 import org.mapstruct.Mapper;
 
 @Mapper
 public class IMandantMapper {
+
+    public Mandant toMandant(MandantDTO mandantDTO) {
+        Mandant mandant = new Mandant();
+        mandant.setName(mandantDTO.getName());
+        mandant.setEmail(mandantDTO.getEmail());
+        return mandant;
+    }
 
 }
