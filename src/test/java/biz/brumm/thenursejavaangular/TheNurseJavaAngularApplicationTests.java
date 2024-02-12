d18cc67c3caeaf90f932fa1b414fac2abd8bc3c9
stbrumm <stbrumm@web.de>
2024-02-12 08:34:46 +0100

    MockMVC-Annotation added

--- src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
+++ src/test/java/biz/brumm/thenursejavaangular/TheNurseJavaAngularApplicationTests.java
@@ -1,9 +1,11 @@
 package biz.brumm.thenursejavaangular;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TheNurseJavaAngularApplicationTests {
 
 	@Test
