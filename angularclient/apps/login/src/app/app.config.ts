a42fc7d6ae94d15ed49a4f06d541402f842a7164
stbrumm <stbrumm@web.de>
2024-02-13 10:37:22 +0100

    aktueller Stand

--- angularclient/apps/login/src/app/app.config.ts
+++ angularclient/apps/login/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(appRoutes)],
