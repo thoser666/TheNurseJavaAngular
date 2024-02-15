e442285ea94e5391c323c7fb3ee3bf5ed560ea6d
stbrumm <stbrumm@web.de>
2024-02-15 08:24:44 +0100

    service added

---
+++ angularclient/apps/angularclient/auth/service/service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angularclient-service',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './service.component.html',
+  styleUrl: './service.component.css',
+})
+export class ServiceComponent {}
