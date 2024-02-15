fefc11b031869ea1fcd2591c24852015f158fc1a
stbrumm <stbrumm@web.de>
2024-02-15 08:06:43 +0100

    authguard

--- angularclient/apps/angularclient/src/app/app.routes.ts
+++ angularclient/apps/angularclient/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import { LoginComponent } from '../../auth/login/login.component';
+import { SignupComponent } from '../../auth/signup/signup.component';
 
 export const appRoutes: Route[] = [
   {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
 ];
