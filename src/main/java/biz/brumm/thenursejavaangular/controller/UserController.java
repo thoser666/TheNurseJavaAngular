3a2751f85ca0591dc4cf155620acd3760312a721
stbrumm <stbrumm@web.de>
2024-02-19 09:26:48 +0100

    Controller done

--- src/main/java/biz/brumm/thenursejavaangular/controller/UserController.java
+++ src/main/java/biz/brumm/thenursejavaangular/controller/UserController.java
@@ -3,11 +3,10 @@ package biz.brumm.thenursejavaangular.controller;
 import biz.brumm.thenursejavaangular.entity.Mandant;
 import biz.brumm.thenursejavaangular.service.IMandantService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +28,41 @@ public class UserController {
     public ResponseEntity<Mandant> find(@PathVariable("id") Long id) {
         Optional<Mandant> item = service.find(id);
         return ResponseEntity.of(item);
+    }
+
+    @PostMapping
+    public ResponseEntity<Mandant> create(@RequestBody Mandant item) {
+        Mandant created = service.create(item);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mandant> update(
+            @PathVariable("id") Long id,
+            @RequestBody Mandant updatedItem) {
+
+        Optional<Mandant> updated = service.update(id, updatedItem);
+
+        return updated
+                .map(value -> ResponseEntity.ok().body(value))
+                .orElseGet(() -> {
+                    Mandant created = service.create(updatedItem);
+                    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(created.getId())
+                            .toUri();
+                    return ResponseEntity.created(location).body(created);
+                });
+    }
+
+    // ✨ New! 👇 DELETE definition ✨
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Mandant> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
