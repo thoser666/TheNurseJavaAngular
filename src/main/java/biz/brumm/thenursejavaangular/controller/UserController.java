60bc145d6084fb1e8362c66a14653774c5c26528
stbrumm <stbrumm@web.de>
2024-02-19 09:19:10 +0100

    Interface filled

--- src/main/java/biz/brumm/thenursejavaangular/controller/UserController.java
+++ src/main/java/biz/brumm/thenursejavaangular/controller/UserController.java
@@ -1,24 +1,32 @@
 package biz.brumm.thenursejavaangular.controller;
 
 import biz.brumm.thenursejavaangular.entity.Mandant;
+import biz.brumm.thenursejavaangular.repository.IMandantRepository;
+import biz.brumm.thenursejavaangular.service.IMandantService;
+import biz.brumm.thenursejavaangular.service.impl.MandantService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
-    // standard constructors
+      private final IMandantService service;
 
-    private IUserRepository userRepository;
-
-    @GetMapping("/users")
-    public List<Mandant> getUsers() {
-        return (List<Mandant>) userRepository.findAll();
+    public UserController(IMandantService service) {
+        this.service = service;
+    }
+    @GetMapping
+    public ResponseEntity<List<Mandant>> findAll() {
+        List<Mandant> items = service.findAll();
+        return ResponseEntity.ok().body(items);
     }
 
-    @PostMapping("/users")
-    void addUser(@RequestBody Mandant mandant) {
-        userRepository.save(mandant);
+    @GetMapping("/{id}")
+    public ResponseEntity<Mandant> find(@PathVariable("id") Long id) {
+        Optional<Mandant> item = service.find(id);
+        return ResponseEntity.of(item);
     }
 }
--- src/main/java/biz/brumm/thenursejavaangular/service/IMandantService.java
+++ src/main/java/biz/brumm/thenursejavaangular/service/IMandantService.java
@@ -1,5 +1,51 @@
 package biz.brumm.thenursejavaangular.service;
 
+import biz.brumm.thenursejavaangular.entity.Mandant;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IMandantService {
+    /**
+     * Find all the Mandant objects.
+     *
+     * @return         	a list of Mandant objects
+     */
+    public List<Mandant> findAll();
+
+    /**
+     * Find a Mandant by its ID.
+     *
+     * @param  id  the ID of the Mandant to find
+     * @return     an Optional containing the found Mandant, or an empty Optional if not found
+     */
+    public Optional<Mandant> find(Long id);
+
+    /**
+     * Creates a new Mandant.
+     *
+     * @param  item	the Mandant object to be created
+     * @return     	the created Mandant object
+     */
+    public Mandant create(Mandant item);
+
+    /**
+     * Update a Mandant with the given id.
+     *
+     * @param  id       the id of the Mandant to update
+     * @param  newItem  the new Mandant object
+     * @return          an Optional containing the updated Mandant, or empty if not found
+     */
+    public Optional<Mandant> update( Long id, Mandant newItem);
+
+    /**
+     * Delete an item by its ID.
+     *
+     * @param  id   the ID of the item to be deleted
+     * @return      void
+     */
+    public void delete(Long id);
+
+
 
 }
