f1f133703ab1c4a9bc65436f0e71641957ed2dcc
stbrumm <stbrumm@web.de>
2024-02-20 09:28:03 +0100

    app-routing debugged

--- angularclient/apps/angularclient/src/app/app-routing.module.ts
+++ angularclient/apps/angularclient/src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminGuard } from './admin/admin.guard';
-import { PanelComponent } from './admin/panel/panel.component';
-import { AuthGuard } from './auth/auth.guard';
-import { LoginGuard } from './auth/login.guard';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { UserProfileComponent } from './profile/user-profile/user-profile/user-profile.component';
-import { ChatComponent } from './chat/chat/chat.component';
-import { InboxComponent } from './chat/inbox/inbox/inbox.component';
-import { HomeComponent } from './home/home.component';
-import { CreatePostComponent } from './post/create-post/create-post.component';
-import { UpdatePostComponent } from './post/update-post/update-post.component';
-import { ViewPostComponent } from './post/view-post/view-post.component';
-import { ChangeProfileComponent } from './profile/change-profile/change-profile.component';
-import { CreateTopicComponent } from './topic/create-topic/create-topic.component';
-import { ListTopicsComponent } from './topic/list-topics/list-topics.component';
-import { AllUsersComponent } from './user/all-users/all-users.component';
-import { TopicPostsComponent } from './post/topic-posts/topic-posts.component';
+import { AdminGuard } from '../../admin/admin.guard';
+import { PanelComponent } from '../../admin/panel/panel.component';
+import { AuthGuard } from '../../auth/auth.guard';
+import { LoginGuard } from '../../auth/login.guard';
+import { LoginComponent } from '../../auth/login/login.component';
+import { SignupComponent } from '../../auth/signup/signup.component';
+import { UserProfileComponent } from '../../profile/user-profile/user-profile/user-profile.component';
+import { ChatComponent } from '../../chat/chat/chat.component';
+import { InboxComponent } from '../../chat/inbox/inbox/inbox.component';
+import { HomeComponent } from '../../home/home.component';
+import { CreatePostComponent } from '../../post/create-post/create-post.component';
+import { UpdatePostComponent } from '../../post/update-post/update-post.component';
+import { ViewPostComponent } from '../../post/view-post/view-post.component';
+import { ChangeProfileComponent } from '../../profile/change-profile/change-profile.component';
+import { CreateTopicComponent } from '../../topic/create-topic/create-topic.component';
+import { ListTopicsComponent } from '../../topic/list-topics/list-topics.component';
+import { AllUsersComponent } from '../../user/all-users/all-users.component';
+import { TopicPostsComponent } from '../../post/topic-posts/topic-posts.component';
 
 const routes: Routes = [
   { path: 'sign-up', component: SignupComponent },
