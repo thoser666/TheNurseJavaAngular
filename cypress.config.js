6fa4cf28cad35341905bc19481224f98a1fcfdcf
thoser666 <thoser666@web.de>
2024-02-26 12:45:25 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   projectId: "xkko6j",
+  fileServerFolder: "./angularclient"
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 };
