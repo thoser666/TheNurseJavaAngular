9e019cbd27adc1f707c7686eba07fab9a0fbaa90
thoser666 <thoser666@web.de>
2024-02-27 12:12:20 +0100

    test added

---
+++ src/test/java/biz/brumm/thenursejavaangular/controller/ApacheKafkaWebControllerTest.java
@@ -0,0 +1,22 @@
+package biz.brumm.thenursejavaangular.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApacheKafkaWebControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void producer() {
+    }
+}
