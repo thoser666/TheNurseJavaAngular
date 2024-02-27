6558e533f6cc499a550be7a7732f79729a065adf
thoser666 <thoser666@web.de>
2024-02-27 14:06:15 +0100

    Greeting added

---
+++ src/main/java/biz/brumm/thenursejavaangular/dto/Greeting.java
@@ -0,0 +1,15 @@
+package biz.brumm.thenursejavaangular.dto;
+
+public class Greeting {
+    private String msg;
+    private String name;
+
+    public Greeting() {
+
+    }
+
+    public Greeting(String msg, String name) {
+        this.msg = msg;
+        this.name = name;
+    }
+}
