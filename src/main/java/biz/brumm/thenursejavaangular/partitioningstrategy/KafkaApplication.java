4ba43312a32d95535022317b46ccd2f244beebf3
thoser666 <thoser666@web.de>
2024-02-28 09:32:51 +0100

    Debugging

--- src/main/java/biz/brumm/thenursejavaangular/partitioningstrategy/KafkaApplication.java
+++ src/main/java/biz/brumm/thenursejavaangular/partitioningstrategy/KafkaApplication.java
@@ -15,7 +15,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-@SpringBootApplication
 public class KafkaApplication {
 
     @Bean
