d97a2e8f5886f1bd9ef8907c68496915d6ab885b
thoser666 <thoser666@web.de>
2024-02-28 07:11:49 +0100

    Kafka done

--- src/main/java/biz/brumm/thenursejavaangular/KafkaProducerConfig.java
+++ src/main/java/biz/brumm/thenursejavaangular/KafkaProducerConfig.java
@@ -1,20 +1,22 @@
-package biz.brumm.thenursejavaangular.config;
+package biz.brumm.thenursejavaangular;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import biz.brumm.thenursejavaangular.dto.Greeting;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
-import java.util.HashMap;
-import java.util.Map;
+@Configuration
+public class KafkaProducerConfig {
 
-public class KafkaProducerConfig
-{
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
