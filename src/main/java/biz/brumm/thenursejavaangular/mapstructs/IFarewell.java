90cf3b019e079f3424c6096e8bc7ebeb1140da30
thoser666 <thoser666@web.de>
2024-03-04 08:23:27 +0100

    CommentDTO added

--- src/main/java/biz/brumm/thenursejavaangular/mapstructs/IFarewell.java
+++ src/main/java/biz/brumm/thenursejavaangular/mapstructs/IFarewell.java
@@ -4,7 +4,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public interface ICommentDto {
-
+public interface IFarewell {
 
 }
---
+++ src/main/java/biz/brumm/thenursejavaangular/mapstructs/dto/FarewellDto.java
@@ -0,0 +1,7 @@
+package biz.brumm.thenursejavaangular.mapstructs.dto;
+
+import lombok.Data;
+
+@Data
+public class FarewellDto {
+}
