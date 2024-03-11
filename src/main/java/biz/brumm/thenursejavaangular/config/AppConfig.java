a8197d384547d401bf746d699f0b5f30156ca29f
thoser666 <thoser666@web.de>
2024-03-11 09:13:27 +0100

    AppConfig is now Primary

--- src/main/java/biz/brumm/thenursejavaangular/config/AppConfig.java
+++ src/main/java/biz/brumm/thenursejavaangular/config/AppConfig.java
@@ -3,10 +3,12 @@ package biz.brumm.thenursejavaangular.config;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "app")
+@Primary
 public class AppConfig {
 
   @NotNull private String url;
