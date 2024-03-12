5e8f62d3500f83e3b1cde7cb70c9ce97b685b42b
thoser666 <thoser666@web.de>
2024-03-12 08:31:55 +0100

    Tests added

--- src/test/java/biz/brumm/thenursejavaangular/controller/UserControllerTestcontainerTest.java
+++ src/test/java/biz/brumm/thenursejavaangular/controller/UserControllerTestcontainerTest.java
@@ -1,7 +1,10 @@
 package biz.brumm.thenursejavaangular.controller;
 
+import biz.brumm.thenursejavaangular.provider.DBConnectionProvider;
 import biz.brumm.thenursejavaangular.service.AuthService;
 import biz.brumm.thenursejavaangular.service.UserService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -9,16 +12,38 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class UserControllerTestcontainerTest {
 
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+          "postgres:16-alpine"
+  );
+
+  UserController userController;
+
+  @BeforeAll
+  static void beforeAll() {
+    postgres.start();
+  }
+
+  @AfterAll
+  static void afterAll() {
+    postgres.stop();
+  }
 
 
   @BeforeEach
-  public void setUp() {
+  void setUp() {
+    DBConnectionProvider connectionProvider = new DBConnectionProvider(
+            postgres.getJdbcUrl(),
+            postgres.getUsername(),
+            postgres.getPassword()
+    );
+    userController = new UserController(connectionProvider);
   }
 
   @Test
