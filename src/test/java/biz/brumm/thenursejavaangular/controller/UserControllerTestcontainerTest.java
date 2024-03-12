bc9c3453eddd5c78d7450b81f9ea8505985b5b85
thoser666 <thoser666@web.de>
2024-03-12 08:04:59 +0100

    testcontainerTest added

---
+++ src/test/java/biz/brumm/thenursejavaangular/controller/UserControllerTestcontainerTest.java
@@ -0,0 +1,100 @@
+package biz.brumm.thenursejavaangular.controller;
+
+import biz.brumm.thenursejavaangular.service.AuthService;
+import biz.brumm.thenursejavaangular.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class UserControllerTestcontainerTest {
+
+
+
+  @BeforeEach
+  public void setUp() {
+  }
+
+  @Test
+  void testFollow() {
+  }
+
+  @Test
+  void testAssignRole() {
+  }
+
+  // Add more tests for other methods in a similar manner
+}
+
+// class UserControllerTest {
+//
+//    @BeforeEach
+//    void setUp() {
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    @Test
+//    void follow() {
+//    }
+//
+//    @Test
+//    void assignRole() {
+//    }
+//
+//    @Test
+//    void unfollow() {
+//    }
+//
+//    @Test
+//    void testUnfollow() {
+//    }
+//
+//    @Test
+//    void getUser() {
+//    }
+//
+//    @Test
+//    void getProfileInfo() {
+//    }
+//
+//    @Test
+//    void getAllSuggestedUsers() {
+//    }
+//
+//    @Test
+//    void getAllReportedUsers() {
+//    }
+//
+//    @Test
+//    void disableUser() {
+//    }
+//
+//    @Test
+//    void enable() {
+//    }
+//
+//    @Test
+//    void getAllFollowersForUser() {
+//    }
+//
+//    @Test
+//    void getAllFollowingForUser() {
+//    }
+//
+//    @Test
+//    void updateUser() {
+//    }
+//
+//    @Test
+//    void handleMyRuntimeException() {
+//    }
+// }
