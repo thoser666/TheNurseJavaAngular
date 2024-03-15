20a416cc2a98a81d864c9d99686b3d3a7887066f
thoser666 <thoser666@web.de>
2024-03-15 07:39:56 +0100

    LoginPageTest started

---
+++ src/test/java/biz/brumm/thenursejavaangular/LoginPageTest.java
@@ -0,0 +1,37 @@
+package biz.brumm.thenursejavaangular;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.Testcontainers;
+
+// page_url = about:blank
+@org.testcontainers.junit.jupiter.Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = LoginPageTest.Initializer.class)
+public class LoginPageTest {
+    public LoginPageTest(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+  static class Initializer
+    implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+      applicationContext.addApplicationListener(
+        (ApplicationListener<WebServerInitializedEvent>)
+          event -> {
+            Testcontainers.exposeHostPorts(event.getWebServer().getPort());
+          });
+    }
+  }
+}
