1c59ab472859ced20d3385b0a05ced00736d5897
thoser666 <thoser666@web.de>
2024-03-18 13:02:34 +0100

    Exception solved

--- src/main/java/biz/brumm/thenursejavaangular/mapstructs/dto/FarewellDto.java
+++ src/main/java/biz/brumm/thenursejavaangular/mapstructs/dto/FarewellDto.java
@@ -3,4 +3,8 @@ package biz.brumm.thenursejavaangular.mapstructs.dto;
 import lombok.Data;
 
 @Data
-public class FarewellDto {}
+public class FarewellDto {
+
+    private String remainingMinutes;
+    private String message;
+}
